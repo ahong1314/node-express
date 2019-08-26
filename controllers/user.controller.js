80c79c5937a3c7e37c7f4e3fdb8a5b4012cf6f30
ahong1314 <jh333338@gmail.com>
2019-08-26 23:35:56 +0800

    user-save

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,10 +4,9 @@ const userModel = require("../models/user.model.js")
 const save = async ( req, res, next ) =>{
     const result = await userModel.save(req.body);
     if(result){
-        res.render('succ', {ret: true, msg: 'succ'})
+        res.render('succ', {ret: true, data: JSON.stringify({msg: 'succ'})})
     }else{
-        res.render('succ', {ret: false, msg: 'fail'})
-
+        res.render('succ', {ret: false, data: JSON.stringify({msg: 'fail'})})
     }
 }
 
